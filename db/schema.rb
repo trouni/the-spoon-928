bd4d85e9349394accdab674e5c97f4a670c88b8f
Douglas Berkley <douglasmberkley@gmail.com>
2022-08-09 21:11:55 +0900

    added category to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_001814) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_121002) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "category"
   end
 
 end
