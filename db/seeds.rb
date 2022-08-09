b7896dd2891514a8d95cdb489092b69fa44d5b3a
Douglas Berkley <douglasmberkley@gmail.com>
2022-08-09 21:09:43 +0900

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Removing the restaurants...'
+Restaurant.destroy_all
+
+chefs = ['Nikki', 'Joshua', 'Yu', 'Bill', 'Daniel', 'Yaya', 'Mounir', 'Soufiane', 'Adam', 'Ayaka', 'Alison', 'Kyle', 'Alex', 'Tony', 'Sae', 'Oanh', 'Andre', 'Mark', 'Leo', 'Jan', 'Koki', 'Malene', 'Carla', 'Song']
+
+puts "Creating #{chefs.count} Restaurants..."
+chefs.shuffle.each do |name|
+  Restaurant.create!(
+    name: "#{name}'s #{Faker::Restaurant.name}",
+    address: Faker::Address.street_address,
+    rating: rand(1..5),
+    category: Faker::Restaurant.type.split.first,
+  )
+end
+puts "...created #{Restaurant.count} restaurants"
