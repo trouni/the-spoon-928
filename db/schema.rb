0fdf3a54c3dfe803694968898279eb1232dc4607
Trouni Tiet <trouni@gmail.com>
2022-08-14 18:58:00 +0900

    Add webpack and minimal template to Doug's app

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_10_004821) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
@@ -23,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_10_004821) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
-    t.integer "restaurant_id", null: false
+    t.bigint "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
